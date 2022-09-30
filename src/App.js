cfe799912069b7c2b3dabd3efa409dcd0a06e12b
GawainTolle <artemsq77@gmail.com>
2022-09-30 09:33:03 +0700

    unit4_EsLint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import Page from "./componets/page";
+import Users from "./components/users";
+import api from "./api";
+import React, { useState } from "react";
+import SearchStatus from "./components/searchStatus";
 
 const App = () => {
-     return <Page />;
+    const [users, setUsers] = useState(api.users.fetchAll());
+    const handleDelete = (id) => {
+        const newUsers = users.filter((user) => user._id !== id);
+        setUsers(newUsers);
+    };
+    const handleToggleBookmark = (id) => {
+        const newUsers = users.map((user) => {
+            if (user._id === id) {
+                return { ...user, bookmark: !user.bookmark };
+            }
+            return user;
+        });
+        setUsers(newUsers);
+    };
+    return (
+        <div>
+            <SearchStatus length={users.length} />
+            <Users
+                users={users}
+                handleDelete={handleDelete}
+                handleToggleBookmark={handleToggleBookmark}
+            />
+        </div>
+    );
 };
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import App from "./app";
 import { createRoot } from "react-dom/client";
-import React from "react";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
+import reportWebVitals from "./reportWebVitals";
+import React from "react";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
 root.render(
-     <React.StrictMode>
-          <App />
-     </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+reportWebVitals();
